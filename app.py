60eb62cf37c64c95154a3dd2872f09c2fa7e252b
Domegal13 <Domegal13cursos@gmail.com>
2024-10-25 18:21:46 -0300

    Se agrega módulo para eliminar Producto

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from src.modulos.funciones import mostrar_menu, menu_switch_case
+from src.modulos.funciones import mostrar_menu, menu_switch_case, limpiar_consola
 from colorama import init, Fore, Style
 init()
 
 opcion_seleccionada = 1000
 
 while opcion_seleccionada != 7:
+    # limpiar_consola()
     mostrar_menu()
     try:
         opcion_seleccionada = int(input(Fore.GREEN + Style.BRIGHT + "Por favor, selecciona una opción (1-7): "  ))
@@ -13,5 +14,4 @@ while opcion_seleccionada != 7:
         print(Fore.RED + Style.BRIGHT + "Opción Inválida...")
 
 
-
-    
+limpiar_consola()
--- src/modulos/funciones.py
+++ src/modulos/funciones.py
@@ -1,12 +1,19 @@
+import os
 import re
 from colorama import init, Fore, Back, Style
 init()
 
-regex_nombre = r'^[A-Za-zÀ-ÿ\s]+$'
+# regex_alpha = r'^[A-Za-zÀ-ÿ\s]+$'
+# regex_nombre = r'^[a-zA-Z0-9\s\W]+$'
+regex_nombre = r'^[a-zA-Z][a-zA-Z0-9\s\W]*$'
 lista_productos = []
 lista_nombres_productos = []
 lista_cant_prod = []
 
+# ########################## FUNCTION CLEAR CONSOLE ###################################################
+
+def limpiar_consola():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 # ########################## FUNCTION VALIDAR NOMBRE ###################################################
 def validar_nombre(nombre):
@@ -49,15 +56,13 @@ def agregar_producto():
         if nombre_producto in lista_nombres_productos:
             print(Fore.YELLOW + f"El producto {nombre_producto} ya existe...")
         else:
-
             try:
                 cantidad_producto = int(input("introduzca la cantidad: "))
                 cantidad_validada = validar_cantidad(cantidad_producto)
                 if cantidad_validada :
                     lista_nombres_productos.append(nombre_producto)
                     lista_cant_prod.append(cantidad_producto)
-                    print("Producto agregado exitosamente...")
-
+                    print(Fore.GREEN + Style.BRIGHT + "\nProducto agregado exitosamente...")
             except ValueError:
                 print(Fore.RED + Style.BRIGHT + "\nLa cantidad debe ser numérica")       
     else:
@@ -73,7 +78,7 @@ def mostrar_un_producto():
         print(Fore.LIGHTGREEN_EX + f"\n{nombre_producto} Stock: {lista_cant_prod[index]}" + Style.RESET_ALL)
     else:
         print(Fore.RED + Style.BRIGHT + f"El producto {nombre_producto}, no existe...")
-
+        
 
               
 # ##########################  OPCION 3 - Actualización: Modificar cantidad de stock del producto ###################################
@@ -147,7 +152,6 @@ def switch_case_stock(opc):
 
 #! ############ FUNCTION MODIFICAR STOTK
 def modificar_stock():
-    
     opc = 0
     while opc != 3:
         print(Fore.YELLOW + Style.BRIGHT + "Menú Modificar Stock") 
@@ -157,10 +161,31 @@ def modificar_stock():
             switch_case_stock(opc)
         except ValueError:
             print(Fore.RED + Style.BRIGHT + "Opción Inválida...")  
-         
+    
+    limpiar_consola()
+        
 
 # ##########################  OPCION 4 -  Elimininación: Dar de baja productos #########################################################
-# TODO: 
+
+def eliminar_producto():
+    nombre_producto = input(Fore.YELLOW + "\n Introduzca el Producto a eliminar: " + Style.RESET_ALL)
+    nombre_producto = nombre_producto.upper()
+    validar_nombre(nombre_producto)
+    if validar_nombre:
+        if nombre_producto in lista_nombres_productos:
+            resp = input(Fore.LIGHTMAGENTA_EX + f"Está seguro de elimiar el producto: " + Fore.BLUE + Style.BRIGHT + f"{nombre_producto}" + Fore.LIGHTMAGENTA_EX  + " - S/N:")
+            if resp == "S" or resp == "s":
+                index = lista_nombres_productos.index(nombre_producto)
+                producto_eliminado = lista_nombres_productos.pop(index)
+                del lista_cant_prod[index]
+            else:
+                pass
+        else:
+            print(Fore.RED + Style.BRIGHT + f"Producto no encontrado..." + Style.RESET_ALL)
+    else:
+        print(Fore.RED + Style.BRIGHT + f"Introduzca un valor correcto..." + Style.RESET_ALL)
+
+
 # ##########################  OPCION 5 - Listado: Listado completo de los productos en la base de datos ###################################
 
 
@@ -169,25 +194,36 @@ def mostrar_productos():
         print(Fore.YELLOW + "\nLista de Productos")
         for nombre in lista_nombres_productos:
             index = lista_nombres_productos.index(nombre)
-            print(Fore.LIGHTGREEN_EX + f"{nombre} Stock: {lista_cant_prod[index]}" + Style.RESET_ALL)
+            # print(Fore.LIGHTGREEN_EX + f"{nombre}" + Fore.YELLOW + f" --Stock: {lista_cant_prod[index]}" + Style.RESET_ALL) 
+            print(Fore.LIGHTGREEN_EX + f"{nombre}" + Fore.YELLOW + " --Stock:" + Fore.GREEN +  f"  {lista_cant_prod[index]}" + Style.RESET_ALL) 
+           
     else:
         print(Fore.RED + Style.BRIGHT + "No hay productos para mostrar")  
         
 
 # ##########################  OPCION 6 - Reporte de bajo stock: Lista de productos con cantidad bajo mínimo ###################################
+
 def mostrar_menu_reporte_stock():
-    print(Fore.YELLOW +  Style.BRIGHT + "\n1- Stock Completo")
+    print(Fore.YELLOW +  Style.BRIGHT + "\n1- Stock Alto")
     print("2- Stock Medio")
     print("3- Stock Bajo")
     print("4- Salir")
 
-def reporte_stock(opc): 
+def reporte_stock(opc):
+    
     if len(lista_nombres_productos) > 0:
         if opc == 1:
+            lista_stock_alto = []
             print(Fore.YELLOW + "\nLista de Productos")
             for nombre in lista_nombres_productos:
                 index = lista_nombres_productos.index(nombre)
-                print(Fore.LIGHTGREEN_EX + f"{nombre} Stock: {lista_cant_prod[index]}" + Style.RESET_ALL)
+                if lista_cant_prod[index] > 10:
+                    lista_stock_alto.append([nombre, lista_cant_prod[index]])
+            if len(lista_stock_alto) > 0:
+                for alto in lista_stock_alto:
+                    print(Fore.LIGHTGREEN_EX + f"{alto[0]} Stock: {alto[1]}" + Fore.LIGHTBLUE_EX + "  -- Stock Alto" + Style.RESET_ALL)   
+            else:
+                print(Fore.RED + Style.BRIGHT + "No hay productos para mostrar")    
         elif opc == 2:
             lista_stock_medio = []
             print(Fore.YELLOW + "\nLista de Productos")
@@ -197,7 +233,7 @@ def reporte_stock(opc):
                     lista_stock_medio.append([nombre, lista_cant_prod[index]])
             if len(lista_stock_medio) > 0:
                 for medio in lista_stock_medio:
-                    print(Fore.LIGHTGREEN_EX + f"{medio[0]} Stock: {medio[1]}" + Fore.YELLOW + "-- Stock Medio" + Style.RESET_ALL)   
+                    print(Fore.LIGHTGREEN_EX + f"{medio[0]} Stock: {medio[1]}" + Fore.YELLOW + "  -- Stock Medio" + Style.RESET_ALL)   
             else:
                 print(Fore.RED + Style.BRIGHT + "No hay productos para mostrar")    
         elif opc == 3:
@@ -209,7 +245,7 @@ def reporte_stock(opc):
                     lista_stock_bajo.append([nombre, lista_cant_prod[index]])
             if len(lista_stock_bajo) > 0:
                 for bajo in lista_stock_bajo:        
-                    print(Fore.LIGHTGREEN_EX + f"{bajo[0]} Stock: {bajo[1]}" + Fore.RED + "-- Stock Bajo" + Style.RESET_ALL)
+                    print(Fore.LIGHTGREEN_EX + f"{bajo[0]} Stock: {bajo[1]}" + Fore.RED + "  -- Stock Bajo" + Style.RESET_ALL)
             else:
                 print(Fore.RED + Style.BRIGHT + "No hay productos para mostrar")  
         elif opc == 4:
@@ -219,10 +255,11 @@ def reporte_stock(opc):
     else:
         print(Fore.RED + Style.BRIGHT + "No hay productos para mostrar")  
 
+
 def mostrar_reporte_productos():
     opc = 0
     while opc != 4:
-        print(Fore.YELLOW + Style.BRIGHT + "\nMostrar Reporte de Stock") 
+        print(Fore.YELLOW + Style.BRIGHT + "\nMostrar Stock") 
         mostrar_menu_reporte_stock()
         try:
             opc = int(input(Fore.YELLOW + Style.BRIGHT + "\nSeleccione una opción 1-4: "))
@@ -230,8 +267,11 @@ def mostrar_reporte_productos():
         except ValueError:
             print(Fore.RED + Style.BRIGHT + "\nOpción Inválida...")  
 
+    limpiar_consola()
 
-# ##########################   SWITCH CASE DE OPCIONES ######################################################################
+
+
+# ##########################   SWITCH CASE OPTIONS MAIN ######################################################################
 def menu_switch_case(opcion_seleccionada):
         match opcion_seleccionada:
             case 1:                
@@ -241,7 +281,7 @@ def menu_switch_case(opcion_seleccionada):
             case 3:
                 modificar_stock()              
             case 4:
-                print("La opcion seleccionada fue: " , opcion_seleccionada, "\n")
+                eliminar_producto()
             case 5:
                 mostrar_productos()
             case 6:
@@ -249,4 +289,4 @@ def menu_switch_case(opcion_seleccionada):
             case 7:
                 print(Style.RESET_ALL + "Saliendo del Sistema, gracias... \n")
             case _:
-                print("La opcion seleccionada es inválida: \n")    
+                print(Fore.RED + Style.BRIGHT + "La opcion seleccionada es inválida: \n")    
